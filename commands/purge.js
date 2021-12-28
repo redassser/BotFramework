5c714cc362ee8aaf7411099f37d2e3b56d4729ad
redassser <redryan890@gmail.com>
2021-12-28 13:37:22 -0500

    purge command bugfixes

--- commands/purge.js
+++ commands/purge.js
@@ -1,12 +1,20 @@
 exports.run = (client,message,array) => {
-    if (!message.member.permissionsIn(message.channel).has("MANAGE_MESSAGES")) {message.channel.send("Sorry! Mods only");return;}
-    if (array.length < 1) {message.channel.send("``.purge [number between 1 and 99]``");return;}
+    if(!message.member.permissions.has("MANAGE_MESSAGES")) {
+        message.channel.send("Sorry! Mods only.")
+        return;
+    }
+	if(!message.guild.me.permissions.has("MANAGE_CHANNELS")) {
+        message.channel.send("Sorry! I don't have the *manage channels* permission.");
+        return;
+    }
     var numb = parseInt(array.shift());
-    if (!Number.isInteger(numb)) {message.channel.send("``.purge [INTEGER between 1 and 99]``");return;}
-    if (numb>99||numb<1) {message.channel.send("``.purge [number BETWEEN 1 and 99]``");return;}
+    if (!Number.isInteger(numb) || numb>99 ||numb<1) {
+		message.channel.send("Please use an integer between 1 and 99.");
+		return;
+	}
     message.channel.bulkDelete(numb+1)
-    .then(messages => message.channel.send(`${messages.size-1} messages be gone`))
-    .catch(console.error);
+    	.then(messages => message.channel.send(`${messages.size-1} messages be gone`))
+    	.catch(console.error);
 }
 exports.desc="Delete this many messages."
-  exports.inp=["[number 1-99]"]
+exports.inp=["[number 1-99]"]
