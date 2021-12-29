99771552b967812f45ad0c3d5ebc18105af1e0ee
redassser <redryan890@gmail.com>
2021-12-29 14:41:52 -0500

    autorole ptach permissions exception

--- commands/poll.js
+++ commands/poll.js
@@ -1,6 +1,14 @@
 exports.run = (client,message,array) => {
     const Discord = require("discord.js");
+    const question = array.join(" ");
     const pollEmbed = new Discord.MessageEmbed()
+        //.setFooter("Account created on ")
+        .setColor(message.member.displayColor)
+        .setDescription(question)
+        .setTitle(message.member.nickname+"'s poll")
+        //.setTimestamp(user.createdAt)
+        //.setThumbnail(user.avatarURL({format:"png",size:512,dynamic:true}))
+        //.setImage(user.bannerURL({format:"png",size:512,dynamic:true}))
 }
 exports.desc = "doesnt work yet"
 exports.inp = ["[question]"]
